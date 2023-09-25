55c020bf3fc8fce40f3aab0bf575bd1b7624c61a
Russell Davis <551404+russelldavis@users.noreply.github.com>
2023-09-24 17:52:48 -0700

    Tweak regexes, notify on errors

--- src/extension.ts
+++ src/extension.ts
@@ -10,14 +10,9 @@ import * as vs from 'vscode';
 const immediateSelectionNameRegex = /^![\d!]*:/;
 let immediateSelectionRange: vs.Range | null = null;
 
-async function executeCommand(name: string): Promise<void> {
-	const success = await vs.commands.executeCommand(name);
-	// if (!success) {throw new Error(`Command "${name}" failed`);}
-}
-
 async function expand(editor: vs.TextEditor) {
 	for(let i = 0; i < 30; i++) {
-		await executeCommand('editor.action.smartSelect.expand');
+		await vs.commands.executeCommand('editor.action.smartSelect.expand');
 		if (!immediateSelectionRange) return;
 		if (editor.selections.length > 1) {
 			throw new Error("immediateSelectionRange should be null where there are multiple selections");
@@ -29,7 +24,7 @@ async function expand(editor: vs.TextEditor) {
 			return;
 		}
 		if (cmp > 0) {
-			// editor.selection = new vs.Selection(immediateSelectionRange.start, immediateSelectionRange.end);
+			editor.selection = new vs.Selection(immediateSelectionRange.start, immediateSelectionRange.end);
 			const msg = "smartSelectFullWords !regex match was preempted by another smartSelect range (possibly from another provider)";
 			vs.window.showWarningMessage(msg);
 			console.log("text:", editor.document.getText(immediateSelectionRange));
@@ -58,62 +53,67 @@ export function activate(context: vs.ExtensionContext) {
 	const config = vs.workspace.getConfiguration("smartSelectFullWords");
 	vs.languages.registerSelectionRangeProvider({pattern: "**"}, {
 		provideSelectionRanges(document, positions) {
-			console.log("provideSelectionRanges");
+			try {
+				console.log("provideSelectionRanges");
+				const isSingleCursor = positions.length === 1;
+				let immediateSelectionName: string | null = null;
+				immediateSelectionRange = null;
+				
+				return positions.map((position) => {
+					const regexes = config.get<Record<string,string>>("regexes", {});
+					const ranges = Object.entries(regexes).flatMap(([name, regexStr]) => {
+						if (!regexStr) return [];
 
-			const isSingleCursor = positions.length === 1;
-			let immediateSelectionName: string | null = null;
-			immediateSelectionRange = null;
-			
-			return positions.map((position) => {
-				const regexes = config.get<Record<string,string>>("regexes", {});
-				const ranges = Object.entries(regexes).flatMap(([name, regexStr]) => {
-					if (!regexStr) return [];
+						const regex = new RegExp(regexStr);
+						const wordRange = document.getWordRangeAtPosition(position, regex);
+						if (!wordRange) return [];
 
-					const regex = new RegExp(regexStr);
-					const wordRange = document.getWordRangeAtPosition(position, regex);
-					if (!wordRange) return [];
+						const wordText = document.getText(wordRange);
+						const match = wordText.match(regex);
+						if (!match) return [];
 
-					const wordText = document.getText(wordRange);
-					const match = wordText.match(regex);
-					if (!match) return [];
-
-					const {start} = wordRange;
-					const resultCol = start.character + match.index!;
-					const range = new vs.Range(start.line, resultCol, start.line, resultCol + match[0].length);
-					if (
-						isSingleCursor &&
-						immediateSelectionNameRegex.test(name) &&
-						(!immediateSelectionName || name < immediateSelectionName)
-					) {
-						immediateSelectionName = name;
-						immediateSelectionRange = range;
-					}
-					console.log("matched:", wordText, "regex:", regexStr);
-					return range;
-				});
-				if (ranges.length === 0) {
-					return new vs.SelectionRange(new vs.Range(0, 0, 0, 0));
-				}
-				ranges.sort(compareRanges);
-				let last: vs.SelectionRange | undefined = undefined;
-				for (const range of ranges) {
-					if (last) {
-						if (last.range.isEqual(range)) continue;
-						if (!last.range.contains(range)) {
-							console.log(
-								"skipping:",
-								document.getText(range),
-								"not contained by:",
-								document.getText(last.range)
-							);
-							continue;
+						const {start} = wordRange;
+						const resultCol = start.character + match.index!;
+						const range = new vs.Range(start.line, resultCol, start.line, resultCol + match[0].length);
+						if (
+							isSingleCursor &&
+							immediateSelectionNameRegex.test(name) &&
+							(!immediateSelectionName || name < immediateSelectionName)
+						) {
+							immediateSelectionName = name;
+							immediateSelectionRange = range;
 						}
+						console.log("matched:", wordText, "regex:", regexStr);
+						return range;
+					});
+					if (ranges.length === 0) {
+						return new vs.SelectionRange(new vs.Range(0, 0, 0, 0));
 					}
-					last = new vs.SelectionRange(range, last);
-					// console.log("providing:", document.getText(range));
-				}
-				return last!;
-			});
+					ranges.sort(compareRanges);
+					let last: vs.SelectionRange | undefined = undefined;
+					for (const range of ranges) {
+						if (last) {
+							if (last.range.isEqual(range)) continue;
+							if (!last.range.contains(range)) {
+								console.log(
+									"skipping:",
+									document.getText(range),
+									"not contained by:",
+									document.getText(last.range)
+								);
+								continue;
+							}
+						}
+						last = new vs.SelectionRange(range, last);
+						// console.log("providing:", document.getText(range));
+					}
+					return last!;
+				});
+			} catch (err) {
+				immediateSelectionRange = null;
+				vs.window.showWarningMessage("Error in provideSelectionRanges (see console)");
+				throw err;
+			}
 		}
 	});
 
